ca41ade8e841f3ec3183a146b169bf3007290a2e
gabrielcursino <gabriel@fuston.com.br>
2017-06-27 22:23:30 -0300

    Adição de imagens, icones e script de highlight de código para inserir na documentação.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 Ion.get(window).on("scroll", function(){
     var scroll = document.body.scrollTop || document.documentElement.scrollTop,
-        logo = Ion.get(".logo");
+        logo = Ion.get(".logo"),
+        logoMini = Ion.get(".logo-mini"),
+        video = Ion.get("video");
 
     if(scroll > 0){
         Ion.get(".toolbar").removeClass("no-elevation");
@@ -11,8 +13,14 @@ Ion.get(window).on("scroll", function(){
 
     if(logo[0].offsetTop - scroll < 0){
         logo.addClass("animated");
+        logoMini.removeClass("hidden");
+        
+        video[0].pause();
     }
     else{
         logo.removeClass("animated");
+        logoMini.addClass("hidden");
+
+        video[0].play();
     }
 })
