89db61f63c8cc313f4fb8a022e2999434eb39529
gabrielcursino <gabriel@fuston.com.br>
2017-07-13 22:24:17 -0300

    Início da documentação.

--- script.js
+++ script.js
@@ -25,4 +25,19 @@ Ion.get(window).on("scroll", function(){
             video[0].play();
         }
     }
-})
+});
+
+Ion.get(".tabs").tabs();
+
+Ion.get(".chips").chips({
+    placeholder: "Add a contact",
+    autocomplete: [
+        {text: "Autocomplete contact", subtitle: "Contact", img: "/img/avatar.jpg"},
+        {text: "John Doe", subtitle: "johndoe@email.com", img: "/img/avatar.jpg"},
+        {text: "Test", subtitle: "Test contact", img: "/img/avatar.jpg"},
+        {text: "Contact2", subtitle: "contact2@email.com", img: "/img/avatar.jpg"},
+        {text: "Deletable contact", subtitle: "deletable", img: "/img/avatar.jpg", deletable: true},
+    ],
+    limit: 4,
+    minlengthSearch: 2
+});
